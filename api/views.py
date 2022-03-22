53b5582b2d9bf57066efff8913e55a19b95e3457
carlosads1998 <carlos.ads1998@gmail.com>
2022-03-22 14:36:09 -0300

    'deploy'

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Video
+from .serializers import videoSerializer
+
+class videoAPIView(generics.ListCreateAPIView):
+    queryset=Video.objects.all()
+    serializer_class=videoSerializer
+class videosAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Video.objects.all()
+    serializer_class=videoSerializer 
